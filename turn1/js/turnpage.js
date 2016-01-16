5be855815740e45f69f10b7206557a2115e8f5d9
sylvialee <sylvialxj@126.com>
2016-01-16 20:28:42 +0800

    no message

---
+++ turn1/js/turnpage.js
@@ -0,0 +1,13 @@
+//turnpage.js
+
+
+
+
+
+$.ajax({
+	type:"GET",
+	dataType:"json",
+	url:"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET",
+	
+})
+
